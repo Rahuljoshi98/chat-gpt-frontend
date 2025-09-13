6475e96d6e039cf63d3046798c1cb42618297606
Rahuljoshi98 <2000rahuljoshi@gmail.com>
2025-09-13 20:53:25 +0530

    login btn added

--- src/Components/Common/SignUpHeader.js
+++ src/Components/Common/SignUpHeader.js
@@ -19,11 +19,11 @@ function SignUpHeader() {
     <>
       <div className="z-10 bg-[#212121] px-6 sticky top-0 left-0 py-2 border-b border-[#ffffff0d] flex justify-between items-center">
         <Logo />
-        <div className="flex items-center">
+        <div className="flex items-center gap-3">
           {/* share btn */}
           <Link
             size="lg"
-            className="hover:bg-[#ffffff1a] px-4 py-2 rounded-3xl text-[16px] items-center gap-1 cursor-pointer sm:flex hidden"
+            className="bg-[#f9f9f9] hover:bg-[#ececec] text-[#0d0d0d] px-4 py-2 rounded-3xl text-[14px] sm:text-[16px] items-center gap-1 cursor-pointer flex"
             href="/sign-in"
           >
             <div className="">Log in</div>
